521c7cba8e094db30e70ce8862b90545a8b0186d
Your Username <you@example.com>
2020-12-22 15:28:47 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,34 @@
+
+
+INSERT INTO users VALUES (1, "Andy", 10);
+INSERT INTO users VALUES (2, "Martha", 20); 
+INSERT INTO users VALUES (3, "Victor", 34); 
+INSERT INTO users VALUES (4, "George", 23);
+INSERT INTO users VALUES (5, "GP", 12);
+INSERT INTO users VALUES (6, "Luke", 13);
+INSERT INTO users VALUES (7, "Kerry", 20);
+INSERT INTO users VALUES (8, "Juliana", 23);
+INSERT INTO users VALUES (9, "Lenny", 23);
+INSERT INTO users VALUES (10, "Jocelyn", 21); 
+INSERT INTO users VALUES (11, "Maria", 9);
+INSERT INTO users VALUES (12, "Jose", 90);
+INSERT INTO users VALUES (13, "Jonathan", 67);
+INSERT INTO users VALUES (14, "Victoria", 45); 
+INSERT INTO users VALUES (15, "Jenny", 32); 
+INSERT INTO users VALUES (16, "Katrina", 21);
+INSERT INTO users VALUES (17, "Richard", 35);
+INSERT INTO users VALUES (18, "Natalia", 11);
+INSERT INTO users VALUES (19, "Heather", 27);
+INSERT INTO users VALUES (20, "Noelia", 22) 
+
+INSERT INTO projects VALUES  (1, "Failing", "project", 18494, '2020-20-5', '2020-10-10');
+INSERT INTO projects VALUES  (2, "Will we pass this?", "project",4737, '2020-20-5', '2020-10-10');
+INSERT INTO projects VALUES  (3, "X Project","project", 76, '2020-20-5', '2020-10-10');
+INSERT INTO projects VALUES  (5, "Project science","project", 1000, '2020-20-5', '2020-10-10'); 
+INSERT INTO projects VALUES  (6, "Project Reggae", "project",67, '2020-20-5', '2020-10-10'); 
+INSERT INTO projects VALUES  (7,"Ikea","project", 18765, '2020-20-5', '2020-10-10');
+INSERT INTO projects VALUES  (8, "Project","project",100, '2020-20-5', '2020-10-10'); 
+INSERT INTO projects VALUES  (4, "Nice","project", 123, '2020-20-5', '2020-10-10');
+INSERT INTO projects VALUES  (9, "Letters","project", 897,  '2020-20-5', '2020-10-10'); 
+INSERT INTO projects VALUES  (10, "Last", "project",16543, '2020-20-5', '2020-10-10')
+
